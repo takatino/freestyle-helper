67cadc0100b33aa82bea07afd6d680ed8c536ca5
Takaya Sato <310takatino@gmail.com>
2021-05-08 17:49:49 +0900

    added function which gets 4 rhyming words randomly

--- main.js
+++ main.js
@@ -6,9 +6,15 @@ canvas.height = window.innerHeight;
 
 const loopspeed = 0;
 
+rhymeDict = {}
+$.get('./rhymedict.json', function(response){
+    rhymeDict = response;
+})
+
+queue = []
 
 function setup() {
-    // setup function
+    
 }
 
 function draw() {
@@ -21,8 +27,25 @@ function update() {
 }
 
 
-function getrhyme(word) {
-    
+function getrhymes(queue) { //stores random rhyming words into queue
+    randkeyint = Math.floor(Math.random() * Object.keys(rhymeDict).length); // random number between 0 ~ num of keys in rhymeDict
+    randkey = Object.keys(rhymeDict)[randkeyint]; // finds key of that index
+    tempRhymes = rhymeDict[randkey];
+
+    randselector = []
+    while (randselector.length < 4 && randselector.length < tempRhymes.length) { // randomly selects 4 (or all if there are less than 4 total words) words
+        x = Math.floor(Math.random() * tempRhymes.length);
+        if (randselector.includes(x)) {
+            continue;
+        }
+        randselector.push(x);
+        
+    }
+    for (i of randselector) {
+        queue.push(tempRhymes[i]);
+    }
+
+    return;
 }
 
 
@@ -36,6 +59,3 @@ let main = setInterval(() => {
 
 }, loopspeed);  
 
-$.get( "http://www.google.com/", function( data ) {
-    console.log(data);     
-});

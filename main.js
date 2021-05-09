a2eb9ad9dc0332d677620f5205003682dc61680d
Takaya Sato <310takatino@gmail.com>
2021-05-09 15:37:47 +0900

    to-do: fix bug with queue and onScreen list

--- main.js
+++ main.js
@@ -6,46 +6,65 @@ canvas.height = window.innerHeight;
 
 const loopspeed = 0;
 
-rhymeDict = {}
-$.get('./rhymedict.json', function(response){
-    rhymeDict = response;
-})
+rhymeDict = {};
+$.get({
+    url:'./rhymedict.json',
+    async: false}, 
+    function(response) {
+        rhymeDict = response;
+    })
 
-queue = []
+
+queue = [];
+onScreen = [];
 
 function setup() {
-    
+    getrhymes();
 }
 
 function draw() {
     ctx.font = "48px serif";
-    ctx.fillText("あ", 0, 48);
+    for (item of onScreen) {
+        ctx.fillText(item[0], item[1], item[2]);
+    }
 }
 
 function update() {
-    //
+
+    if (queue.length < 12) {
+        getrhymes();
+    }
+
+    if (onScreen.length < 4) {
+        onScreen.push([queue.pop(), Math.floor(Math.random() * canvas.width), 0]);
+    }
+
+    for (item of onScreen) {
+        item[2] += 1;
+        if (item[2] > canvas.height + 100) {
+            onScreen.pop(item);
+        }
+    }
 }
 
 
-function getrhymes(queue) { //stores random rhyming words into queue
-    randkeyint = Math.floor(Math.random() * Object.keys(rhymeDict).length); // random number between 0 ~ num of keys in rhymeDict
+function getrhymes() { //stores random rhyming words into queue
+    randkeyint = Math.floor(Math.random() * (Object.keys(rhymeDict).length)); // random number between 0 ~ num of keys in rhymeDict
     randkey = Object.keys(rhymeDict)[randkeyint]; // finds key of that index
     tempRhymes = rhymeDict[randkey];
-
-    randselector = []
-    while (randselector.length < 4 && randselector.length < tempRhymes.length) { // randomly selects 4 (or all if there are less than 4 total words) words
+    randselector = [];
+    
+    while (randselector.length < 4) { // randomly selects 4 (or all if there are less than 4 total words) words
         x = Math.floor(Math.random() * tempRhymes.length);
         if (randselector.includes(x)) {
             continue;
         }
         randselector.push(x);
-        
     }
+    
     for (i of randselector) {
         queue.push(tempRhymes[i]);
     }
-
-    return;
 }
 
 

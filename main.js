6a04690ca500b7cb5779a51716a12426b40bb8e9
Takaya Sato <310takatino@gmail.com>
2021-05-10 19:54:05 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const loopspeed = 0;
+const loopspeed = 10;
+
+const maxwords_onscreen = Math.floor(0.00004 * canvas.width * canvas.height);
+const fontsize = Math.floor(0.035 * canvas.width);
 
 rhymeDict = {};
 $.get({
@@ -19,30 +22,36 @@ queue = [];
 onScreen = [];
 
 function setup() {
-    getrhymes();
+    for (i = 0; i < 10; i++) {
+        getrhymes();
+    }
 }
 
 function draw() {
-    ctx.font = "48px serif";
+    ctx.font = fontsize + "px serif";
     for (item of onScreen) {
+        ctx.globalAlpha = 0.6;
         ctx.fillText(item[0], item[1], item[2]);
     }
+    ctx.globalAlpha = 1.0;
 }
 
 function update() {
-
-    if (queue.length < 12) {
+    while (queue.length < 12) {
         getrhymes();
     }
 
-    if (onScreen.length < 4) {
-        onScreen.push([queue.pop(), Math.floor(Math.random() * canvas.width), 0]);
+    while (onScreen.length < maxwords_onscreen) {
+        newword = queue.pop()
+        onScreen.push([newword, Math.floor(Math.random() * (canvas.width - 48*newword.length)), Math.floor(Math.random() * -canvas.height), 0.5*Math.random()+0.5]);
+        //[wordstring, x, y, speed]
     }
 
-    for (item of onScreen) {
-        item[2] += 1;
-        if (item[2] > canvas.height + 100) {
-            onScreen.pop(item);
+    tempOSL = onScreen.length;
+    for (i = tempOSL - 1; i >= 0; i--) {
+        onScreen[i][2] += onScreen[i][3];
+        if (onScreen[i][2] > canvas.height + 100) {
+            onScreen.splice(i, 1);
         }
     }
 }
@@ -54,7 +63,7 @@ function getrhymes() { //stores random rhyming words into queue
     tempRhymes = rhymeDict[randkey];
     randselector = [];
     
-    while (randselector.length < 4) { // randomly selects 4 (or all if there are less than 4 total words) words
+    while (randselector.length < 8 && randselector.length < tempRhymes.length) { // randomly selects 4 (or all if there are less than 4 total words) words
         x = Math.floor(Math.random() * tempRhymes.length);
         if (randselector.includes(x)) {
             continue;
@@ -62,6 +71,7 @@ function getrhymes() { //stores random rhyming words into queue
         randselector.push(x);
     }
     
+
     for (i of randselector) {
         queue.push(tempRhymes[i]);
     }
